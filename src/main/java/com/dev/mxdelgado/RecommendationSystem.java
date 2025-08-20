f74f56082076e2b7a294398b36124c96925475e3
Marlon Xavier Delgado Ruiz <marlonxavi6@gmail.com>
2025-08-20 14:32:13 -0500

    add: se genera el  metodo getRecommendationsByGenre, el cual genera las recomendaciones al usuario con base a las peliculas que ya haya visto

--- src/main/java/com/dev/mxdelgado/RecommendationSystem.java
+++ src/main/java/com/dev/mxdelgado/RecommendationSystem.java
@@ -2,6 +2,7 @@ package com.dev.mxdelgado;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 
 public class RecommendationSystem {
@@ -23,6 +24,19 @@ public class RecommendationSystem {
         return movies.stream()
             .filter(m -> m.getGenre().equals(genre))
             .toList();
+    }
+
+    public List<Movie> getRecommendationsByGenre(String genre) {
+        return movies.stream()
+        .filter(m -> m.getGenre().equals(genre))
+        .filter(RecommendationSystem::clasificate)
+        .sorted(Comparator.comparingDouble(Movie::getRating).reversed()
+                .thenComparing(Movie::getTitle))
+        .toList();
+    }
+
+    private static boolean clasificate(Movie m) {
+        return m.getRating() > 4.0 && m.getVotes() >= 100;
     }
 
 }
