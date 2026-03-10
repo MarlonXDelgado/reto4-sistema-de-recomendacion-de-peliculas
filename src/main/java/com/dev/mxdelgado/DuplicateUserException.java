fee2a4254a4480705a19e93f0b283de239aa72c4
MARLON XAVIER DELGADO RUIZ <marlonxavi6@gmil.com>
2026-03-10 11:45:45 -0500

    add: se documenta la clase de exepcion personalizada DuplicateUserException.java

--- src/main/java/com/dev/mxdelgado/DuplicateUserException.java
+++ src/main/java/com/dev/mxdelgado/DuplicateUserException.java
@@ -1,15 +1,32 @@
 package com.dev.mxdelgado;
 
+/**
+ * Excepción lanzada cuando se intenta crear un usuario duplicado.
+ * Se lanza cuando se intenta realizar la creacion de un usuario identico gramaticalmente hablando.
+ */
+
 public class DuplicateUserException extends Exception {
 
+    /**
+     * Constructor por defecto.
+     */
     public DuplicateUserException(){
         super("El nombre de usuario ya está registrado. Intente con otro nombre.");
     }
 
+    /**
+     * Constructor con mensaje personalizado.
+     * @param message El mensaje de error.
+     */
     public DuplicateUserException(String message){
         super(message);
     }
 
+    /**
+     * Constructor con mensaje y causa.
+     * @param message El mensaje de error.
+     * @param cause La causa de la excepción.
+     */
     public DuplicateUserException(String message, Throwable causa){
         super(message,causa);
     }
